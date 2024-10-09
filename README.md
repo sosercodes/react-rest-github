8d64c5f960ab705feef913eb1cc3c2b01f4e7704
Simon Oser <simon.oser@gmail.com>
2024-10-09 18:04:30 +0200

    Adds React REST Github App
    Adds github call

--- README.md
+++ README.md
@@ -49,4 +49,70 @@ export default defineConfig({
 
 ## Remove unused things
 
+Remove the unused things.
+
+## Add Axios
+
+You can get repositories with a certain _keyword_g using the ithub api.
+
+```bash
+curl https://api.github.com/search/repositories?q=react
+```
+
+## The 'Repository' result
+
+The result contains an array with the following fields.
+
+```javascript
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+  description: string;
+};
+```
+
+## Add some state for result and keyword
+
+```typscript
+const [repos, setRepos] = useState<Repository[]>([]);
+const [keyword, setKeyword] = useState('');
+```
+
+## Add Input field and button
+
+Create a form for the search term.
+
+```typescript
+<form onSubmit={handleFormSubmit}>
+  <input value={keyword} onChange={e => setKeyword(e.target.value)}  />
+  <button type="submit" onSubmit={handleFormSubmit}>Fetch</button>
+</form>
+```
+
+## Use Axios
+
+Now add the `handleFormSubmit`.
+
+
+```typescript
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+  axios.get<{ items: Repository[] }>(`https://api.github.com/search/repositories?q=${keyword}`)
+  .then(response => setRepos(response.data.items))
+  .catch(err => console.error(err));
+}
+```
+
+## Add the result
+
+Now we need a div where we put the result.
+
+```typescript
+<div key={repo.id} className='card'>
+    <h2>{repo.full_name}</h2>
+    <a href={repo.html_url}>{repo.html_url}</a>
+    <p>{repo.description}</p>
+</div>
+```
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type Repository = {
+  id: number;
+  full_name: string;
+  html_url: string;
+  description: string;
+};
 
+function App() {
+  const [repos, setRepos] = useState<Repository[]>([]);
+  const [keyword, setKeyword] = useState('');
+
+  const handleFormSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    axios.get<{ items: Repository[] }>(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => setRepos(response.data.items))
+    .catch(err => console.error(err));
+  }
+  
   return (
     <>
-      <h1>React REST Github App</h1>
-      <div className="counter">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    
+      <div className='card'>
+        <h1>React REST Github App</h1>
+
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor='keyword'>Keyword</label>
+          <input id="keyword" name="keyword" value={keyword} onChange={e => setKeyword(e.target.value)}  />
+          <button type="submit" >Fetch</button>
+        </form>
       </div>
+      {repos.length === 0 ? (
+        <div className='card'>
+        <p>No data available</p>
+        </div>
+      ) : (
+        <>
+            {repos.map((repo) => (
+            <div key={repo.id} className='card'>
+               <h2>{repo.full_name}</h2>
+               <a href={repo.html_url}>{repo.html_url}</a>
+               <p>{repo.description}</p>
+            </div>
+            ))}
+        </>
+      )}
     </>
   )
 }
